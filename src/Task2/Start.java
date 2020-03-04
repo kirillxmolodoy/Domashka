d3f3be63dfe3f84ca4b91b7d01815f02cf8f12bf
molodoy <shchegl2001@yandex.ru>
2020-03-04 14:52:08 +0300

    Dz

--- src/Task2/Start.java
+++ src/Task2/Start.java
@@ -1,5 +1,9 @@
 package Task2;
 
+import Task2.Obstacles.Cross;
+import Task2.Obstacles.Obstacle;
+import Task2.Obstacles.Wall;
+
 public class Start {
     public static void main(String[] args) {
         Obstacle[] obstacles ={new Wall("5"), new Cross("100")};
